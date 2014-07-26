506da1fa3b92b504b44bf06945bbac4ecb21d3e2
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2014-07-25 23:36:52 -0700

    Need these

---
+++ include/logger/flags.h
@@ -0,0 +1,13 @@
+/*
+ * RaceCapture: flags.h
+ *
+ *  Created on: Jun 10, 2014
+ *      Author: stieg
+ */
+
+#ifndef FLAGS_H_
+#define FLAGS_H_
+
+#define FLAG_BIT(i) (1 << i)
+
+#endif /* FLAGS_H_ */
