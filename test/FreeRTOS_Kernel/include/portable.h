732b50ba7ee82baeebb5d49518651c4da87cf1b0
Jean-Sebastien Stoezel <js.stoezel@gmail.com>
2015-01-14 20:27:24 -0600

    Added xPortGetFreeHeapSize macro

--- test/FreeRTOS_Kernel/include/portable.h
+++ test/FreeRTOS_Kernel/include/portable.h
@@ -358,6 +358,7 @@ void *pvPortMalloc( size_t xSize ) PRIVILEGED_FUNCTION;
 void vPortFree( void *pv ) PRIVILEGED_FUNCTION;
 void vPortInitialiseBlocks( void ) PRIVILEGED_FUNCTION;
 size_t xPortGetFreeHeapSize( void ) PRIVILEGED_FUNCTION;
+#define portGetFreeHeapSize xPortGetFreeHeapSize
 void * pvPortRealloc( void *pv, size_t xWantedSize );
 
 /*
